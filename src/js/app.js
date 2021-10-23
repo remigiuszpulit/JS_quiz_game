ba02f3a44068d6a7f54eb11f6e3d4d4b89aa4c35
remigiuszpulit <remigiusz.pulit@gmail.com>
2021-10-23 23:54:03 +0200

    added start and finish and question randomization

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,23 @@ import { questionSource } from "./questions.js";
 
 const ansCount = [];
 let temp = "";
+const nums = new Set();
+let qSet = [];
+
+const start = createTag({
+  tagName: "h1",
+  // className
+  idName: "welcome",
+  text: "Welcome to the Friends Quiz!",
+});
+
+const startBtn = createTag({
+  tagName: "button",
+  className: ["btn", "btn-primary"],
+  text: "Launch",
+});
+
+start.appendChild(startBtn);
 
 const questionHTML = (questions, number) => {
   const id = questions[number];
@@ -11,6 +28,7 @@ const questionHTML = (questions, number) => {
   const questionTitleTag = createTag({
     tagName: "h2",
     // className: ["dupa"],
+    idName: "qst",
     text: title,
   });
 
@@ -18,26 +36,20 @@ const questionHTML = (questions, number) => {
 
   answers.forEach((answer) => {
     const { ansTitle, value } = answer;
-    const ansTag = createTag({
-      tagName: "h3",
-      //   className
-      text: ansTitle,
-    });
 
     const ansBtn = createTag({
       tagName: "button",
-      // className
-      text: "Select",
+      className: ["btn", "btn-secondary"],
+      text: ansTitle,
     });
 
     ansBtn.addEventListener("click", () => {
       let ansVal = value;
       temp = `${ansVal}`;
-      console.log(temp);
+      //   console.log(temp);
     });
-    ansTag.appendChild(ansBtn);
 
-    ansArr.push(ansTag);
+    ansArr.push(ansBtn);
   });
   //remember to randomize ansArr here
   ansArr.forEach((element) => {
@@ -47,6 +59,84 @@ const questionHTML = (questions, number) => {
   return questionTitleTag;
 };
 
-// questionHTML(questionSource, 2);
+const nextBtn = createTag({
+  tagName: "button",
+  className: ["btn", "btn-primary"],
+  idName: "nextBtn",
+  text: "Next",
+});
 
-document.querySelector("body").appendChild(questionHTML(questionSource, 3));
+nextBtn.addEventListener("click", () => {
+  ansCount.push(temp);
+  let q = document.getElementById("qst");
+  if (q !== null) {
+    q.remove();
+  }
+
+  document
+    .querySelector("body")
+    .appendChild(questionHTML(questionSource, qSet[1]));
+  qSet.shift();
+  console.log(ansCount);
+  temp = "";
+  if (ansCount.length === 4) {
+    document.querySelector("body").appendChild(finishBtn);
+    let b = document.getElementById("nextBtn");
+    b.remove();
+  } else {
+    document.querySelector("body").appendChild(nextBtn);
+  }
+});
+
+startBtn.addEventListener("click", () => {
+  let s = document.getElementById("welcome");
+  s.remove();
+  while (nums.size !== 6) {
+    nums.add(Math.floor(Math.random() * 6));
+  }
+  qSet = [...nums];
+  console.log(qSet);
+
+  document
+    .querySelector("body")
+    .appendChild(questionHTML(questionSource, qSet[1]));
+  qSet.shift();
+  document.querySelector("body").appendChild(nextBtn);
+});
+
+const f = (arr, val) => {
+  let count = 0;
+  arr.forEach((v) => v === val && count++);
+  return count;
+};
+
+const scoreboardTag = (no) => {
+  const tag = createTag({
+    tagName: "h2",
+    // className
+    text: `You have answered ${no} questions correctly`,
+  });
+  return tag;
+};
+
+const finishBtn = createTag({
+  tagName: "button",
+  className: ["btn", "btn-primary"],
+  text: "Finish",
+  idName: "finishBtn",
+});
+
+finishBtn.addEventListener("click", () => {
+  ansCount.push(temp);
+  let q = document.getElementById("qst");
+  if (q !== null) {
+    q.remove();
+  }
+  const score = f(ansCount, "true");
+
+  document.querySelector("body").appendChild(scoreboardTag(score));
+  let fin = document.getElementById("finishBtn");
+  fin.remove();
+});
+
+document.querySelector("body").appendChild(start);
